c8de2ca242cd402e0b5d2335cdbeb1edf51b1901
Andrew <a@fandb.pro>
2019-01-25 19:36:40 +0300

    callback hell sample and refactoring added

--- DAY5/promise/app.js
+++ DAY5/promise/app.js
@@ -1,3 +1,7 @@
+// ****************************
+// ***** SAMPLE OF PROMISSES
+// ****************************
+
 // var p1 = new Promise(function(resolve, reject){
 //   var num = Math.random();
 //   if(num > 0.5){
@@ -13,13 +17,88 @@
 //   console.log('Error: ', error);
 // });
 
+// ****************************
+// ***** SAMPLE OF PROMISSES
+// ****************************
+
+// var promise = new Promise(function(resolve, reject){
+//   setTimeout(function(){
+//     var randomInt = Math.floor(Math.random()*10);
+//     resolve(randomInt);
+//   }, 3000);
+// });
+
+// promise.then(function(data){
+//   console.log('Random int passed to resolve: ', data);
+// });
+
+// ****************************
+// ***** NESTED PROMISSES
+// ****************************
+
 var promise = new Promise(function(resolve, reject){
   setTimeout(function(){
-    var randomInt = Math.floor(Math.random()*10);
+    randomInt = Math.floor(Math.random()*10);
     resolve(randomInt);
   }, 3000);
 });
 
 promise.then(function(data){
   console.log('Random int passed to resolve: ', data);
+  return new Promise(function(resolve, reject){
+    setTimeout(function(){
+      resolve(Math.floor(Math.random()*10));
+    }, 1000);
+  }).then(function(data){
+    console.log('Second random int passed to resolve: ', data)
+  });
+});
+
+
+// ****************************************************************************
+// ***** MORE UGLY EXAMPLES NESTED CALLBACKS
+// ****************************************************************************
+
+var counter = 0;
+setTimeout(function(){
+  counter++;
+  console.log('Counter: ', counter);
+  setTimeout(function(){
+    counter++;
+    console.log('Counter: ', counter);
+    setTimeout(function(){
+      counter++;
+      console.log('Counter: ', counter);
+    }, 3000);
+  }, 2000);
+}, 1000);
+
+
+
+// ****************************************************************************
+// ***** MORE UGLY EXAMPLES TO REFACTOR NESTED CALLBACKS
+// ****************************************************************************
+
+var counter = 0;
+function incCounter(){
+  counter++;
+  console.log('Counter: ', counter);
+}
+
+function runLater(callback, timeInMs){
+  var p = new Promise(function(resolve, reject){
+    setTimeout(function(){
+      var res = callback();
+      resolve(res);
+    }, timeInMs);
+  });
+  return p;
+}
+
+runLater(incCounter, 1000).then(function(){
+  return runLater(incCounter, 2000);
+}).then(function(){
+  return runLater(incCounter, 3000);
+}).then(function(){
+  console.log('Stop your counting!!!');
 });
