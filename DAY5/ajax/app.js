5849decb129f450b9bee2adab184c6c9affb61a8
Andrew <a@fandb.pro>
2019-01-25 23:50:31 +0300

    Ajax starter with GitHub ZEN API

---
+++ DAY5/ajax/app.js
@@ -0,0 +1,18 @@
+var XHR = new XMLHttpRequest();
+
+var url = 'https://api.github.com/zen';
+
+XHR.onreadystatechange = function(){
+  if(XHR.readyState === 4){
+    if(XHR.status === 200){
+      header.innerText = XHR.responseText;
+    } else {
+      header.innerText = 'Some problem happened'
+    }
+  };
+}
+
+XHR.open('GET', url);
+XHR.send();
+
+var header = document.querySelector('.xhr');
