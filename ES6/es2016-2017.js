f0e651ee119acd2ca24ae1363888037c45058f56
Andrew <a@fandb.pro>
2019-04-03 07:02:36 +0300

    working on asic functions

--- ES6/es2016-2017.js
+++ ES6/es2016-2017.js
@@ -40,3 +40,68 @@ nums.includes(44); // false
 // The first parameter is the total length of the new string
 // The second parameter is what to pad with from the end. the default is empty space
 "awesome".padEnd(10, '!'); // "awesome!!!"
+
+// **********************
+// ES2017 Async Functions
+// **********************
+// A special kind of functions that is created using word async
+// The purpose of async functions is to simplify writing asynchronouse code , specifically Promises
+async function first() {
+    return 'We did it';
+}
+
+first(); // return Promise {<resolved>: "We did it"}
+first().then(val => console.log(val));
+
+// Await
+// A reserved keyword that can only be executed inside async functions
+// await pouses the executions of the async functionand is followed by a Promise. The await keyword waits for the promise to resolve, and then resumes the the async function execution and returns the resolved value
+// We think of the await keyword like a puse button (simular to yield with generators);
+
+async function getMovieDate() {
+    console.log('Starting')
+    var movieData = await $.getJSON('https://omdbapi.com?t=titanic&apikey=thewdb');
+    console.log('All done')
+    console.log(movieData);
+}
+getMovieDate(); // Will return
+// Starting
+// Promise {<pending>}
+// All done
+// {Title: "Titanic", Year: "1997", Rated: "PG-13", Released: "19 Dec 1997", Runtime: "194 min", …}
+
+// No need of .then, callbacks or yield
+
+// Object async
+var movieCollector = {
+    data: 'titanic',
+    async getMovie() {
+        var response = await $.getJSON(`https://omdbapi.com?t=${this.data}&apikey=thewdb`);
+        console.log(response);
+    }
+}
+
+// Class async
+// We can also place async functions as instance method with ES2015 class syntax
+class movieData {
+    constructor(name) {
+        this.name = name;
+    }
+    async getMovie() {
+        var response = await $.getJSON(`https://omdbapi.com?t=${this.name}&apikey=thewdb`);
+        console.log(response);
+    }
+}
+
+var m = new movieData('shrek');
+m.getMovie(); //{Title: "Shrek", Year: "2001", Rated: "PG", Released: "18 May 2001", Runtime: "90 min", …}
+
+// Handling errors 
+async function getUser(user) {
+    try {
+        var response = await $.getJSON(`https://api.github.com/users/${user}`);
+        console.log(response.name);
+    } catch {
+        console.log('User doesn\'t exist');
+    }
+}
