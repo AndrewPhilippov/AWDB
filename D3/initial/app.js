58434e86f796f51f03fa42a11fcd9d2b64274d12
Andrew <a@fandb.pro>
2019-04-04 07:51:50 +0300

    Split D3 into diff brunches

--- D3/initial/app.js
+++ D3/initial/app.js
@@ -1,28 +1,28 @@
-d3.select('#page-title');
-d3.selectAll('li');
-
-// I should not do like this...
-d3.selectAll('li')._groups[0]; // !!NO
-
-// to select multiple elements we have to use * selection.nodes() *
-d3.selectAll('li').nodes();
-// to select single element we have to use * selection.node() *
-d3.selectAll('li').node();
-
-d3.select('#page-title')
-    .style("background-color", "#00feab")
-    .style("color", "#ffffff")
-    .attr("class", "new-class")
-    .text("D3 IS COOL!!!");
-
-// There are 
+// Event Listeners in D3
 /*
-* style,
-* attr,
-* text,
-* html
-METHODS which can help us to get or to set values to element/elements
-Working with classes we can use 'classed' method
-The syntax is:
-.classed("new-class", true) 
+selection.on(eventType, callback)
+*/
+
+d3.select("h1").on("click", function () {
+    console.log("Event listners are sweet!")
+})
+
+// To remove listner we have to pass null instead of callback function
+d3.select("h1").on("click", null);
+
+d3.select("#new-note").on("submit", function () {
+    // How do we accept the event object???
+    d3.event.preventDefault();
+
+    var input = d3.select("input");
+    d3.select("#notes")
+        .append("p")
+        .classed("note", true)
+        .text(input.property("value"));
+    input.property("value", "")
+});
+
+/*
+To remove elements we use .remove() method
+d3.selectAll("p").remove();
 */
