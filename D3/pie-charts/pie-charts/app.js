448a9e71e720de9f3e2d589556809367ab7f775f
Andrew <a@fandb.pro>
2019-04-16 08:23:43 +0300

    Working on pieCharts. Have to fix arc padding problem

--- D3/pie-charts/pie-charts/app.js
+++ D3/pie-charts/pie-charts/app.js
@@ -1,17 +1,17 @@
-var minYear = d3.min(birthData, d => d.year);
-var width = 600;
-var height = 600;
-var yearData = birthData.filter(d => d.year === minYear);
+const minYear = d3.min(birthData, d => d.year);
+const maxYear = d3.max(birthData, d => d.year);
+const width = 600;
+const height = 600;
 
-var continents = [];
-for (var i = 0; i < birthData.length; i++) {
-    var continent = birthData[i].continent;
+const continents = [];
+for (let i = 0; i < birthData.length; i++) {
+    const continent = birthData[i].continent;
     if (continents.indexOf(continent) === -1) {
         continents.push(continent);
     }
 }
 
-var colorScale = d3.scaleOrdinal()
+const colorScale = d3.scaleOrdinal()
     .domain(continents)
     .range(d3.schemeCategory10);
 
@@ -22,20 +22,43 @@ d3.select('svg')
     .attr('transform', 'translate(' + width / 2 + ', ' + height / 2 + ')')
     .classed('chart', true);
 
-var arcs = d3.pie()
-    .value(d => d.births)
-    (yearData);
+makeGraph(minYear);
 
-var path = d3.arc()
-    .outerRadius(width / 2 - 10)
-    .innerRadius(width / 4);
+d3.select('input')
+    .property('min', minYear)
+    .property('max', maxYear)
+    .property('value', minYear)
+    .on('input', function(){
+        makeGraph(+d3.event.target.value)
+    });
 
-d3.select('.chart')
-    .selectAll('.arc')
-    .data(arcs)
-    .enter()
-    .append('path')
-    .classed('arc', true)
-    .attr('fill', d => colorScale(d.data.continent))
-    .attr('stroke', 'black')
-    .attr('d', path);
+function makeGraph(year){
+    const yearData = birthData.filter(d => d.year === year);
+    const arcs = d3.pie()
+        .value(d => d.births)
+        .sort(function(a, b){
+            if(a.continent < b.continent) return -1;
+            else if(a.continent > b.continent) return 1;
+            else return a.births - b.births;
+        })
+        (yearData);
+    const path = d3.arc()
+        .outerRadius(width / 2 - 10)
+        .innerRadius(width / 4)
+        .padAngle(.02);
+
+    const update = d3.select('.chart')
+        .selectAll('.chart')
+        .data(arcs);
+    update
+        .exit()
+        .remove();
+    update
+        .enter()
+        .append('path')
+            .classed('arc', true)
+        .merge(update)
+            .attr('fill', d => colorScale(d.data.continent))
+            .attr('stroke', 'black')
+            .attr('d', path);
+}
