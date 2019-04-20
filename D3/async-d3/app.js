32e53715646a49de581296382abe58ab412f3f80
Andrew <a@fandb.pro>
2019-04-20 22:19:45 +0300

    Working on async d3 calls, have to fix getting cities info

--- D3/async-d3/app.js
+++ D3/async-d3/app.js
@@ -1,1 +1,33 @@
-// write your code here!
+d3.queue()
+    .defer(d3.json, "./countries.json")
+    .defer(d3.csv, "./simplemaps-worldcities-basic.csv", function(row){
+        if(+row.pop < 10000 ) return;
+        return {
+            cityName: row.city,
+            countryCode: row.iso2,
+            population: +row.pop
+        }
+    })
+    .awaitAll(function(error, allData){
+        if(error) throw error;
+        const data = allData[0].geonames.map(country => {
+            country.cities = allData[1].filter(city => city.counryCode === country.countryCode);
+            return country;
+        });
+        const countrySelection = d3.select("body")
+            .selectAll("div")
+            .data(data)
+            .enter()
+            .append("div");
+
+        countrySelection
+            .append("h3")
+                .text(d => d.countryName);
+
+        countrySelection
+            .append("ul")
+            .html(d => d.cities.map(city => {
+                const percentage = city.population / d.population * 100;
+                return `<li>${city.cityName} - ${percentage.toFixed(2)}%</li>`
+            }))
+    });
